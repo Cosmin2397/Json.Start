888ac665f4a7fd0cd29cfa67ef3c4ef2061da50e
Cosmin2397 <59839882+Cosmin2397@users.noreply.github.com>
2022-02-26 09:09:57 +0200

    Add method to json string and test for json facts
    Add a method which check if input contains unrecognized escape chars and add a test for this method.
    
    Co-Authored-By: alinirimias <7344351+alinirimias@users.noreply.github.com>

--- Json.Validator/JsonString.cs
+++ Json.Validator/JsonString.cs
@@ -6,6 +6,13 @@ namespace Json
     {
         public static bool IsJsonString(string input)
         {
+            return HaveTwoQuotes(input)
+                && ContainsControlChars(input)
+                && ContainsInvalidEscapeChar(input);
+        }
+
+        static bool ContainsControlChars(string input)
+        {
             string[] controlChars = { "\n", "\r" };
             foreach (string c in controlChars)
             {
@@ -13,6 +20,37 @@ namespace Json
                 {
                     return false;
                 }
+            }
+
+            return true;
+        }
+
+        static bool ContainsInvalidEscapeChar(string input)
+        {
+            const string escapeChar = "\"\\/fnbrtu";
+
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                int x = 0;
+                if (input[i] == '\\')
+                {
+                    x++;
+                }
+
+                if ((x > 0 && escapeChar.Contains(input[i + 1])) || input.IndexOf('\\') == -1)
+                    {
+                        return true;
+                    }
+            }
+
+            return false;
+        }
+
+        static bool HaveTwoQuotes(string input)
+        {
+            if (input == null)
+            {
+                return false;
             }
 
             return input.StartsWith("\"") && input.EndsWith("\"") && input.Length > 1;
