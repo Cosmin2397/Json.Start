6f1bbfe8a6aa029044e4511eac9da83105446787
Cosmin2397 <59839882+Cosmin2397@users.noreply.github.com>
2022-04-04 21:51:24 +0300

    Test if soccer teams method which check if a team have fewer points and method which check if two teams have same names

--- TeamRanks/TeamsRank/RankingTests/SoccerTeamFacts.cs
+++ TeamRanks/TeamsRank/RankingTests/SoccerTeamFacts.cs
@@ -18,5 +18,22 @@ namespace RankingTests
             string expected = "1. Steaua 25";
             Assert.Equal(actual, expected);
         }
+
+        [Fact]
+        public static void CheckIfaTeamHaveMorePointsThanOtherTeam()
+        {
+            SoccerTeam team = new SoccerTeam("Steaua", 25);
+            SoccerTeam team2 = new SoccerTeam("Dinamo", 20);
+            bool actual = team.HaveFewerPoints(team2);
+            Assert.False(actual);
+        }
+
+        [Fact]
+        public static void CheckIfaTeamHaveSameNameAsOtherTeam()
+        {
+            SoccerTeam team = new SoccerTeam("Steaua", 25);
+            bool actual = team.IsTeamName("Steaua");
+            Assert.True(actual);
+        }
     }
 }
