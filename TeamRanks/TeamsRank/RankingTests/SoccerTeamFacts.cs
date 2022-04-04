a559e2140c58791e57e9a62c27fa0b21c0fcd1f9
Cosmin2397 <59839882+Cosmin2397@users.noreply.github.com>
2022-04-04 21:31:41 +0300

    Test if soccer class show a team

--- TeamRanks/TeamsRank/RankingTests/SoccerTeamFacts.cs
+++ TeamRanks/TeamsRank/RankingTests/SoccerTeamFacts.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
+using Ranking;
 
 namespace RankingTests
 {
-    internal class SoccerTeamFacts
+    public class SoccerTeamFacts
     {
+        [Fact]
+        public static void ShowATeam()
+        {
+            SoccerTeam team = new SoccerTeam("Steaua", 25);
+            string actual = team.ShowTeam(0);
+            string expected = "1. Steaua 25";
+            Assert.Equal(actual, expected);
+        }
     }
 }
