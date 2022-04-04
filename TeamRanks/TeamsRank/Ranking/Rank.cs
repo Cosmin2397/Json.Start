2e52bf638ee387f823e9aed45d1cb36904ceeaaf
Cosmin2397 <59839882+Cosmin2397@users.noreply.github.com>
2022-04-04 21:22:16 +0300

    Create teams array in rank class

--- TeamRanks/TeamsRank/Ranking/Rank.cs
+++ TeamRanks/TeamsRank/Ranking/Rank.cs
@@ -4,5 +4,11 @@ namespace Ranking
 {
     public class Rank
     {
+        SoccerTeam[] teams;
+
+        public Rank(SoccerTeam[] teams)
+        {
+            this.teams = teams;
+        }
     }
 }
