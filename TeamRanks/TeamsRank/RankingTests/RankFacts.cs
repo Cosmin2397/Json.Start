e0e4fc6b9b1e486a5f547a87ef39f347b1ef7019
Cosmin2397 <59839882+Cosmin2397@users.noreply.github.com>
2022-04-04 21:44:36 +0300

    Add a test to check if add new team method works

--- TeamRanks/TeamsRank/RankingTests/RankFacts.cs
+++ TeamRanks/TeamsRank/RankingTests/RankFacts.cs
@@ -19,5 +19,24 @@ namespace RankingTests
             string expected = "1. Steaua 25";
             Assert.Equal(actual, expected);
         }
+
+        [Fact]
+        public static void AddNewTeam()
+        {
+            SoccerTeam[] teamsRank = new SoccerTeam[2]
+        {
+            new SoccerTeam("Steaua", 25),
+            new SoccerTeam("Dinamo", 20)
+        };
+            Rank rank = new(teamsRank);
+            string[] actual = rank.AddNewTeam("Rapid", 26);
+            string[] expected =
+            {
+            "1. Rapid 26",
+            "2. Steaua 25",
+            "3. Dinamo 20",
+            };
+            Assert.Equal(actual, expected);
+        }
     }
 }
