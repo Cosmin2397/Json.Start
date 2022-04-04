56a5315577081d211f4052a86246db19d10cb378
Cosmin2397 <59839882+Cosmin2397@users.noreply.github.com>
2022-04-04 21:26:54 +0300

    Add a test to check if get right team by position in rank

--- TeamRanks/TeamsRank/RankingTests/RankFacts.cs
+++ TeamRanks/TeamsRank/RankingTests/RankFacts.cs
@@ -6,6 +6,18 @@ namespace RankingTests
 {
     public class RankFacts
     {
-        
+        [Fact]
+        public static void GetTeamByPosition_WithATwoTeamsRank_Unmodified()
+        {
+            SoccerTeam[] teamsRank = new SoccerTeam[2]
+        {
+            new SoccerTeam("Steaua", 25),
+            new SoccerTeam("Dinamo", 20)
+        };
+            Rank rank = new Rank(teamsRank);
+            string actual = rank.GetTeamByPosition(1);
+            string expected = "1. Steaua 25";
+            Assert.Equal(actual, expected);
+        }
     }
 }
