0ae9c0c0caf10372b3664b49fec4f940e6bf5b61
Cosmin2397 <59839882+Cosmin2397@users.noreply.github.com>
2022-04-18 18:55:56 +0300

    Test if work with null reference

---
+++ JsonApp/JsonAppTests/RangeTests.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JsonApp;
+using Xunit;
+
+
+namespace JsonAppTests
+{
+    public class RangeTests
+    {
+        [Fact]
+        public static void CheckIfWork_WithNull()
+        {
+            Range range = new('a', 'f');
+            Assert.False(range.Match(null));
+        }
+
+     }
+}
