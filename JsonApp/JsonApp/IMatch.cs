39884cd8e34307a140c3609fd419e4dc66083207
Cosmin2397 <59839882+Cosmin2397@users.noreply.github.com>
2022-04-19 11:20:45 +0300

    Create IMatch interface

---
+++ JsonApp/JsonApp/IMatch.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonApp
+{
+    interface IMatch
+    {
+        bool Success();
+        string RemainingText();
+    }
+}
