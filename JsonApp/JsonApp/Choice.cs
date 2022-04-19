125da97df7453fdffac387485a462ff6638c1082
Cosmin2397 <59839882+Cosmin2397@users.noreply.github.com>
2022-04-19 10:53:03 +0300

    Add choice class with match method, add tests with null and empty shouldn't work.

---
+++ JsonApp/JsonApp/Choice.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonApp
+{
+    public class Choice
+    {
+        private IPattern[] patterns;
+
+        public Choice(params IPattern[] patterns)
+        {
+            this.patterns = patterns;
+        }
+
+        public bool Match(string text)
+        {
+            return true;
+        }
+    }
+}
---
+++ JsonApp/JsonAppTests/ChoiceTests.cs
@@ -0,0 +1,30 @@
+﻿using Xunit;
+using JsonApp;
+
+namespace JsonAppTests
+{
+    public class ChoiceTests
+    {
+        [Fact]
+        public static void CheckIfWork_WithNull()
+        {
+            Choice choices = new(
+                new Character('0'),
+                new Range('1', '9')
+                );
+
+            Assert.False(choices.Match(null));
+        }
+
+        [Fact]
+        public static void CheckIfWork_WithStringEmpty()
+        {
+            Choice choices = new(
+                new Character('0'),
+                new Range('1', '9')
+                );
+
+            Assert.False(choices.Match(string.Empty));
+        }
+    }
+}
