443ccfc1f1a907a2ccbf17be1a370819005a4d84
Cosmin2397 <59839882+Cosmin2397@users.noreply.github.com>
2022-04-19 10:44:38 +0300

    Add more tests for valid and invalid values

--- JsonApp/JsonAppTests/CharacterTests.cs
+++ JsonApp/JsonAppTests/CharacterTests.cs
@@ -25,5 +25,30 @@ namespace JsonAppTests
             Character character = new('a');
             Assert.False(character.Match("z"));
         }
+
+        [Fact]
+        public static void Check_WithAValidChar_ShouldReturnTrue()
+        {
+            Character character = new('a');
+            Assert.True(character.Match("a"));
+        }
+
+        [Fact]
+        public static void Check_WithAStringValidChar_ShouldReturnTrue()
+        {
+            Character character = new('a');
+            Assert.True(character.Match("abcd"));
+            Assert.True(character.Match("abc"));
+            Assert.True(character.Match("acd"));
+        }
+
+        [Fact]
+        public static void Check_WithAStringInvalidChar_ShouldReturnFalse()
+        {
+            Character character = new('a');
+            Assert.False(character.Match("gbcd"));
+            Assert.False(character.Match("1bc"));
+            Assert.False(character.Match("!cd"));
+        }
     }
 }
