6ccc897a9aa6cebf5e4a3007fd40384b756073b1
Cosmin2397 <59839882+Cosmin2397@users.noreply.github.com>
2022-04-19 10:46:01 +0300

    Create IPattern interface

---
+++ JsonApp/JsonApp/IPattern.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonApp
+{
+    public interface IPattern
+    {
+        bool Match(string text);
+    }
+}
