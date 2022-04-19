f0ab47b1c71fe9a4a667fccf6a26fb615bec1d7d
Cosmin2397 <59839882+Cosmin2397@users.noreply.github.com>
2022-04-19 10:39:38 +0300

    Create character class and test with null or empty shouldn't work

---
+++ JsonApp/JsonApp/Character.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonApp
+{
+    public class Character
+    {
+        readonly char pattern;
+
+        public Character(char pattern)
+        {
+            this.pattern = pattern;
+        }
+
+        public bool Match(string text)
+        {
+            return true;
+        }
+    }
+}
---
+++ JsonApp/JsonAppTests/CharacterTests.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+using JsonApp;
+
+namespace JsonAppTests
+{
+    public class CharacterTests
+    {
+        [Fact]
+        public static void CheckIfWork_WithNull()
+        {
+            Character character = new('a');
+            Assert.False(character.Match(null));
+        }
+
+        [Fact]
+        public static void CheckIfWork_WithEmpty()
+        {
+            Character character = new('a');
+            Assert.False(character.Match(string.Empty));
+        }
+    }
+}
