882b2f50aaff2d2bda046c9d5a398d87cd30b78b
Cosmin2397 <59839882+Cosmin2397@users.noreply.github.com>
2022-04-22 15:22:43 +0300

    Refactoring range tests

--- JsonApp/JsonAppTests/RangeTests.cs
+++ JsonApp/JsonAppTests/RangeTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,55 +15,55 @@ namespace JsonAppTests
         public static void CheckIfWork_WithNull()
         {
             Range range = new('a', 'f');
-            Assert.False(range.Match(null));
+            Assert.False(range.Match(null).Success());
         }
 
         [Fact]
         public static void CheckIfWork_WithEmpty()
         {
             Range range = new('a', 'f');
-            Assert.False(range.Match(string.Empty));
+            Assert.False(range.Match(string.Empty).Success());
         }
 
         [Fact]
         public static void Check_WithAValidChar_ShouldReturnTrue()
         {
             Range range = new('a', 'f');
-            Assert.True(range.Match("a"));
+            Assert.True(range.Match("a").Success());
         }
 
         [Fact]
         public static void Check_WithAInvalidChar_ShouldReturnFalse()
         {
             Range range = new('a', 'f');
-            Assert.False(range.Match("z"));
+            Assert.False(range.Match("z").Success());
         }
 
         [Fact]
         public static void Check_WithAStringValidChar_ShouldReturnTrue()
         {
             Range range = new('a', 'f');
-            Assert.True(range.Match("fbcd"));
-            Assert.True(range.Match("abc"));
-            Assert.True(range.Match("bcd"));
+            Assert.True(range.Match("fbcd").Success());
+            Assert.True(range.Match("abc").Success());
+            Assert.True(range.Match("bcd").Success());
         }
 
         [Fact]
         public static void Check_WithAStringInvalidChar_ShouldReturnFalse()
         {
             Range range = new('a', 'f');
-            Assert.False(range.Match("gbcd"));
-            Assert.False(range.Match("1bc"));
-            Assert.False(range.Match("!cd"));
+            Assert.False(range.Match("gbcd").Success());
+            Assert.False(range.Match("1bc").Success());
+            Assert.False(range.Match("!cd").Success());
         }
 
         [Fact]
         public static void Check_WithAnInvalidRAnge_ShouldReturnFalse()
         {
             Range range = new('f', 'a');
-            Assert.False(range.Match("abcd"));
-            Assert.False(range.Match("fbc"));
-            Assert.False(range.Match("bcd"));
+            Assert.False(range.Match("abcd").Success());
+            Assert.False(range.Match("fbc").Success());
+            Assert.False(range.Match("bcd").Success());
         }
 
     }
