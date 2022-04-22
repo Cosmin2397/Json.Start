2a0a0d7e6a32055a8885f8b5b30a7674cea8ea19
Cosmin2397 <59839882+Cosmin2397@users.noreply.github.com>
2022-04-22 15:17:37 +0300

    Add match class

---
+++ JsonApp/JsonApp/Match.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonApp
+{
+    public class Match : IMatch
+    {
+        readonly bool success;
+        readonly string message;
+
+        public Match(bool success, string message)
+        {
+            this.success = success;
+            this.message = message;
+        }
+        public bool Success()
+        {
+            return success;
+        }
+        public string RemainingText()
+        {
+            if(Success())
+            {
+                return message[1..];
+            }
+
+            return message;
+        }
+    }
+}
