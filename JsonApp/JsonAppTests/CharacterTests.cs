44cfc5585cfcadce89a3a7d1fd77ebbc8b30a278
Cosmin2397 <59839882+Cosmin2397@users.noreply.github.com>
2022-04-22 15:20:24 +0300

    Refactoring character tests

--- JsonApp/JsonAppTests/CharacterTests.cs
+++ JsonApp/JsonAppTests/CharacterTests.cs
@@ -9,46 +9,49 @@ namespace JsonAppTests
         public static void CheckIfWork_WithNull()
         {
             Character character = new('a');
-            Assert.False(character.Match(null));
+            Assert.False(character.Match(null).Success());
         }
 
         [Fact]
         public static void CheckIfWork_WithEmpty()
         {
             Character character = new('a');
-            Assert.False(character.Match(string.Empty));
+            Assert.False(character.Match(string.Empty).Success());
         }
 
         [Fact]
         public static void Check_WithAInvalidChar_ShouldReturnFalse()
         {
             Character character = new('a');
-            Assert.False(character.Match("z"));
+            Assert.False(character.Match("z").Success());
         }
 
         [Fact]
         public static void Check_WithAValidChar_ShouldReturnTrue()
         {
             Character character = new('a');
-            Assert.True(character.Match("a"));
+            Assert.True(character.Match("a").Success());
         }
 
         [Fact]
         public static void Check_WithAStringValidChar_ShouldReturnTrue()
         {
             Character character = new('a');
-            Assert.True(character.Match("abcd"));
-            Assert.True(character.Match("abc"));
-            Assert.True(character.Match("acd"));
+            Assert.True(character.Match("abcd").Success());
+            Assert.True(character.Match("abcd").RemainingText() == "bcd");
+            Assert.True(character.Match("abc").Success());
+            Assert.True(character.Match("abc").RemainingText() == "bc");
+            Assert.True(character.Match("acd").Success());
+            Assert.True(character.Match("acd").RemainingText() == "cd");
         }
 
         [Fact]
         public static void Check_WithAStringInvalidChar_ShouldReturnFalse()
         {
             Character character = new('a');
-            Assert.False(character.Match("gbcd"));
-            Assert.False(character.Match("1bc"));
-            Assert.False(character.Match("!cd"));
+            Assert.False(character.Match("gbcd").Success());
+            Assert.False(character.Match("1bc").Success());
+            Assert.False(character.Match("!cd").Success());
         }
     }
 }
