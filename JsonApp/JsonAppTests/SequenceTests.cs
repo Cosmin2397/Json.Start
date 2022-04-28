41117beef7a831416ab5aa5dd9f7815d5dc36445
Cosmin2397 <59839882+Cosmin2397@users.noreply.github.com>
2022-04-28 16:24:48 +0300

    Add test to work with invalid values

--- JsonApp/JsonAppTests/SequenceTests.cs
+++ JsonApp/JsonAppTests/SequenceTests.cs
@@ -28,5 +28,36 @@ namespace JsonAppTests
             Assert.False(match.Success());
         }
 
+        [Fact]
+        public static void CheckIfWork_WithInvalidNumbers()
+        {
+            Sequence sequence = new(
+                new Character('0'),
+                new Range('1', '9')
+                );
+
+            Assert.False(sequence.Match("a12").Success());
+            Assert.True(sequence.Match("a12").RemainingText() == "a12");
+            Assert.False(sequence.Match("m12").Success());
+            Assert.True(sequence.Match("m12").RemainingText() == "m12");
+            Assert.False(sequence.Match("0a2").Success());
+            Assert.True(sequence.Match("0a2").RemainingText() == "0a2");
+        }
+
+        [Fact]
+        public static void CheckIfWork_WithInvalidLetters()
+        {
+            var ab = new Sequence(
+                new Character('a'),
+                new Character('b')
+               );
+
+            Assert.False(ab.Match("def").Success());
+            Assert.True(ab.Match("def").RemainingText() == "def");
+            Assert.False(ab.Match("").Success());
+            Assert.True(ab.Match("").RemainingText() == "");
+            Assert.False(ab.Match(null).Success());
+            Assert.True(ab.Match(null).RemainingText() == null);
+        }
     }
 }
