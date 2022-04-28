01b647f0ac35c324b3c85a706cdcd6dbfd8fa01f
Cosmin2397 <59839882+Cosmin2397@users.noreply.github.com>
2022-04-28 18:03:59 +0300

    Add test for invalid string

--- JsonApp/JsonAppTests/AnyTests.cs
+++ JsonApp/JsonAppTests/AnyTests.cs
@@ -23,6 +23,25 @@ namespace JsonAppTests
                 Assert.False(accepted.Match(string.Empty).Success());
                 Assert.True(accepted.Match(string.Empty).RemainingText() == string.Empty);
             }
+
+            [Fact]
+            public static void Check_WithAInvalidString_ShouldReturnFalse()
+            {
+                Any accepted = new("Ee");
+                Assert.False(accepted.Match("a").Success());
+            }
+
+            [Fact]
+            public static void Check_WithAInvalidSignString_ShouldReturnTrue()
+            {
+                Any accepted = new("+-");
+                Assert.False(accepted.Match("2").Success());
+                Assert.True(accepted.Match("2").RemainingText() == "2");
+                Assert.False(accepted.Match("*3").Success());
+                Assert.True(accepted.Match("*3").RemainingText() == "*3");
+                Assert.False(accepted.Match("/4").Success());
+                Assert.True(accepted.Match("/4").RemainingText() == "/4");
+            }
         }
     }
 }
