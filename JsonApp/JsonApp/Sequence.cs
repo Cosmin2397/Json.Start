7220a2ffb71704bf429ec83f8b1c8d5d32f0a8f9
Cosmin2397 <59839882+Cosmin2397@users.noreply.github.com>
2022-04-28 16:21:01 +0300

    Add Sequence class and test for null or empty values

---
+++ JsonApp/JsonApp/Sequence.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonApp
+{
+    public class Sequence : IPattern
+    {
+        readonly IPattern[] patterns;
+
+        public Sequence(params IPattern[] patterns)
+        {
+            this.patterns = patterns;
+        }
+
+        public IMatch Match(string text)
+        {
+            foreach (var pattern in patterns)
+            {
+                return pattern.Match(text);
+            }
+
+            return new Match(true, text);
+
+        }
+    }
+}
---
+++ JsonApp/JsonAppTests/SequenceTests.cs
@@ -0,0 +1,32 @@
+﻿using Xunit;
+using JsonApp;
+
+namespace JsonAppTests
+{
+    public class SequenceTests
+    {
+        [Fact]
+        public static void CheckIfWork_WithNull()
+        {
+            Sequence sequence = new(
+                new Character('0'),
+                new Range('1', '9')
+                );
+            IMatch match = sequence.Match(null);
+            bool isMatch = match.Success();
+            Assert.False(isMatch);
+        }
+
+        [Fact]
+        public static void CheckIfWork_WithStringEmpty()
+        {
+            Sequence sequence = new(
+                new Character('0'),
+                new Range('1', '9')
+                );
+            IMatch match = sequence.Match(string.Empty);
+            Assert.False(match.Success());
+        }
+
+    }
+}
