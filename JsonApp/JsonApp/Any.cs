41d4b86aa986fe26676a9f334435224424b051fd
Cosmin2397 <59839882+Cosmin2397@users.noreply.github.com>
2022-04-28 18:02:23 +0300

    Add Any class, add match method for empty and null and test it

---
+++ JsonApp/JsonApp/Any.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonApp
+{
+    public class Any: IPattern
+    {
+        private readonly string _accepted;
+
+        public Any(string accepted)
+        {
+            _accepted = accepted;
+        }
+
+        public IMatch Match(string text)
+        {
+            return new Match(false, text);
+        }
+    }
+}
---
+++ JsonApp/JsonAppTests/AnyTests.cs
@@ -0,0 +1,28 @@
+﻿
+namespace JsonAppTests
+{
+    using Xunit;
+    using JsonApp;
+
+    namespace JsonAppTests
+    {
+        public class AnyTests
+        {
+            [Fact]
+            public static void CheckIfWork_WithNull()
+            {
+                Any accepted = new("Ee");
+                Assert.False(accepted.Match(null).Success());
+                Assert.True(accepted.Match(null).RemainingText() == null);
+            }
+
+            [Fact]
+            public static void CheckIfWork_WithEmpty()
+            {
+                Any accepted = new("Ee");
+                Assert.False(accepted.Match(string.Empty).Success());
+                Assert.True(accepted.Match(string.Empty).RemainingText() == string.Empty);
+            }
+        }
+    }
+}
