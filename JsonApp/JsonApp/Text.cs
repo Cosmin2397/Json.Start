871f371fbe4aee6497bded4c6293bd9cfebe8d4d
Cosmin2397 <59839882+Cosmin2397@users.noreply.github.com>
2022-05-03 15:08:33 +0300

    Add text class and test for null or empty text

---
+++ JsonApp/JsonApp/Text.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonApp
+{
+    public class Text : IPattern
+    {
+        private readonly string prefix;
+        public Text(string prefix)
+        {
+            this.prefix = prefix;
+        }
+
+        public IMatch Match(string text)
+        {
+            if (!string.IsNullOrEmpty(text))
+            {
+                return new Match(true, text);
+            }
+
+            return new Match(false, text);
+        }
+    }
+
+}
---
+++ JsonApp/JsonAppTests/TextTests.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+using JsonApp;
+
+namespace JsonAppTests
+{
+    public class TextTests
+    {
+        [Fact]
+        public static void CheckIfWork_WithNull()
+        {
+            Text accepted = new("true");
+            Assert.False(accepted.Match(null).Success());
+            Assert.True(accepted.Match(null).RemainingText() == null);
+        }
+
+        [Fact]
+        public static void CheckIfWork_WithEmpty()
+        {
+            Text accepted = new("true");
+            Assert.False(accepted.Match(string.Empty).Success());
+            Assert.True(accepted.Match(string.Empty).RemainingText() == string.Empty);
+        }
+    }
+}
