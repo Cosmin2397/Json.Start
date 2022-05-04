6f9f28f650afd341816a1f026191c25f77b17b76
Cosmin2397 <59839882+Cosmin2397@users.noreply.github.com>
2022-05-04 16:49:20 +0300

    Add many class and tests for empty and null

---
+++ JsonApp/JsonApp/Many.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonApp
+{
+    public class Many : IPattern
+    {
+        readonly IPattern pattern;
+
+        public Many(IPattern pattern)
+        {
+            this.pattern = pattern;
+        }
+
+        public IMatch Match(string text)
+        {
+            return new Match(true, text);
+        }
+    }
+}
---
+++ JsonApp/JsonAppTests/ManyTests.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+using JsonApp;
+
+namespace JsonAppTests
+{
+    public  class ManyTests
+    {
+        [Fact]
+        public static void CheckIfWork_WithNull()
+        {
+            Many many = new( new Character('a'));
+            Assert.True(many.Match(null).Success());
+            Assert.True(many.Match(null).RemainingText() == null);
+        }
+
+        [Fact]
+        public static void CheckIfWork_WithStringEmpty()
+        {
+            Many many = new(new Character('a'));
+            Assert.True(many.Match(string.Empty).Success());
+            Assert.True(many.Match(string.Empty).RemainingText() == string.Empty);
+        }
+    }
+}
