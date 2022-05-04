45cd4c57aa4ab35eb26b24c75bccb2b4bdb28c1f
Cosmin2397 <59839882+Cosmin2397@users.noreply.github.com>
2022-05-04 19:17:31 +0300

    Add optional class and tests for empty and null

---
+++ JsonApp/JsonApp/Optional.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonApp
+{
+    public class Optional : IPattern
+    {
+        readonly IPattern pattern;
+
+        public Optional(IPattern pattern)
+        {
+            this.pattern = pattern;
+        }
+
+        public IMatch Match(string text)
+        {
+                return new Match(true, text);
+        }
+    }
+}
---
+++ JsonApp/JsonAppTests/OptionalTests.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+using JsonApp;
+
+namespace JsonAppTests
+{
+    public class OptionalTests
+    {
+        [Fact]
+        public static void CheckIfWork_WithNull()
+        {
+            Optional optional = new(new Character('a'));
+            Assert.True(optional.Match(null).Success());
+            Assert.Equal(optional.Match(null).RemainingText(), null);
+        }
+
+        [Fact]
+        public static void CheckIfWork_WithStringEmpty()
+        {
+            Optional optional = new(new Character('a'));
+            Assert.True(optional.Match(string.Empty).Success());
+            Assert.Equal(optional.Match(string.Empty).RemainingText(), string.Empty);
+        }
+    }
+}
