d636b69f9fb99d78e08d8680cacd7ea6a1f84a4f
Cosmin2397 <59839882+Cosmin2397@users.noreply.github.com>
2022-05-05 16:47:42 +0300

    Test match method with valid range values

--- JsonApp/JsonAppTests/OneOrMoreTests.cs
+++ JsonApp/JsonAppTests/OneOrMoreTests.cs
@@ -15,5 +15,16 @@ namespace JsonAppTests
             Assert.False(one.Match(inputString).Success());
             Assert.Equal(one.Match(inputString).RemainingText(), expectedString);
         }
+
+        [Theory]
+        [InlineData("123", "")]
+        [InlineData("1a", "a")]
+        [InlineData("1913cb1a", "cb1a")]
+        public static void CheckIfWork_WithValidRangeValues(string inputString, string expectedString)
+        {
+            OneOrMore one = new(new Range('0', '9'));
+            Assert.True(one.Match(inputString).Success());
+            Assert.Equal(one.Match(inputString).RemainingText(), expectedString);
+        }
     }
 }
