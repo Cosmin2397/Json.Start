3346e7b0a478adb0cf0bd50d302351be79dce317
Cosmin2397 <59839882+Cosmin2397@users.noreply.github.com>
2022-05-05 16:45:52 +0300

    Add OneOrMore class and test if work with null, empty or invalid values

---
+++ JsonApp/JsonApp/OneOrMore.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonApp
+{
+    public class OneOrMore : IPattern
+    {
+        private readonly IPattern pattern;
+
+
+
+        public OneOrMore(IPattern pattern)
+        {
+            this.pattern = pattern;
+        }
+
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+    }
+}
---
+++ JsonApp/JsonAppTests/OneOrMoreTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+using JsonApp;
+
+namespace JsonAppTests
+{
+    public class OneOrMoreTests
+    {
+        [Theory]
+        [InlineData(null,null)]
+        [InlineData("","")]
+        [InlineData("bc", "bc")]
+        public static void CheckIfWork_WithInvalidRangeValues(string inputString, string expectedString)
+        {
+            OneOrMore one = new(new Range('0', '9'));
+            Assert.False(one.Match(inputString).Success());
+            Assert.Equal(one.Match(inputString).RemainingText(), expectedString);
+        }
+    }
+}
