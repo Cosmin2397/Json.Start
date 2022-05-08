81e495ea00ca1aa6c5985f6c6095929a7127ff5c
Cosmin2397 <59839882+Cosmin2397@users.noreply.github.com>
2022-05-08 21:04:12 +0300

    Add test to check class

--- JsonApp/JsonAppTests/ListTests.cs
+++ JsonApp/JsonAppTests/ListTests.cs
@@ -9,6 +9,9 @@ namespace JsonAppTests
         [InlineData(null, null)]
         [InlineData("", "")]
         [InlineData("abc", "abc")]
+        [InlineData("1,2,3", "")]
+        [InlineData("1,2,3,", ",")]
+        [InlineData("1a", "a")]
         public static void CheckIfWork_WithInvalidRangeValues(string inputString, string expectedString)
         {
             List one = new(new Range('0', '9'), new Character(','));
