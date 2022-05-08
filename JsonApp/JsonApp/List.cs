a110471759cba62d86ff48d171546518d5815d33
Cosmin2397 <59839882+Cosmin2397@users.noreply.github.com>
2022-05-08 21:01:05 +0300

    Add List class, and add test for empty and null

---
+++ JsonApp/JsonApp/List.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonApp
+{
+    public class List:IPattern
+    {
+        private readonly IPattern pattern;
+
+        public List(IPattern element, IPattern separator)
+        {
+            this.pattern = new Many(new Choice(element, separator));
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+    }
+}
---
+++ JsonApp/JsonAppTests/ListTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+using JsonApp;
+
+namespace JsonAppTests
+{
+    public class ListTests
+    {
+        [Theory]
+        [InlineData(null, null)]
+        [InlineData("", "")]
+        public static void CheckIfWork_WithInvalidRangeValues(string inputString, string expectedString)
+        {
+            List one = new(new Range('0', '9'), new Character(','));
+            Assert.True(one.Match(inputString).Success());
+            Assert.Equal(expectedString, one.Match(inputString).RemainingText());
+        }
+
+    }
+}
