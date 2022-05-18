586c43b3af62cf5fedb25bdab3c177277e990ba1
Cosmin2397 <59839882+Cosmin2397@users.noreply.github.com>
2022-05-18 16:26:54 +0300

    Update number class

--- JsonApp/JsonApp/Number.cs
+++ JsonApp/JsonApp/Number.cs
@@ -12,19 +12,16 @@ namespace JsonApp
 
         public Number()
         {
-            Character minus = new('-');
-            Character dot = new('.');
-            Any exponential = new("eE");
             Any signs = new("+-");
             Any empty = new("");
             Range startNum = new('1', '9');
             Range digits = new('0', '9');
             Character zero = new('0');
-            Sequence zeroDotSeq = new(new Optional(minus), zero);
+            Sequence zeroDotSeq = new(new Optional(new Character('-')), zero);
             Sequence zeroSeq = new(zero, empty);
             Sequence fullNum = new(new Optional(signs), startNum, new Many(digits));
-            Sequence exponentialSeq = new(exponential, new Optional(signs), new OneOrMore(digits), empty);
-            Sequence dotSeq = new(new Choice(zeroDotSeq, fullNum), dot, new OneOrMore(digits), new Choice(exponentialSeq, empty));
+            Sequence exponentialSeq = new(new Any("eE"), new Optional(signs), new OneOrMore(digits), empty);
+            Sequence dotSeq = new(new Choice(zeroDotSeq, fullNum), new Character('.'), new OneOrMore(digits), new Choice(exponentialSeq, empty));
             Sequence wholeNumSeq = new(new Choice(fullNum, zeroSeq), new Choice(exponentialSeq, empty));
             pattern = new Choice(dotSeq, wholeNumSeq);
         }
