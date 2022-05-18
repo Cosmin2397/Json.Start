b08414657ff2e2ec85efb7da205b19371a5b4823
Cosmin2397 <59839882+Cosmin2397@users.noreply.github.com>
2022-05-18 16:12:36 +0300

    Create number class, and add test for null

---
+++ JsonApp/JsonApp/Number.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonApp
+{
+    public class Number : IPattern
+    {
+        private readonly IPattern pattern;
+
+        public Number()
+        {
+            pattern = new Choice();
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text); 
+        }
+
+    }
+
+    }
---
+++ JsonApp/JsonAppTests/NumberTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+using JsonApp;
+
+namespace JsonAppTests
+{
+    public class NumberTests
+    {
+        [Theory]
+        [InlineData(null)]
+        public static void CheckIfWork_WithInvalidValues(string inputString)
+        {
+            Number num = new();
+            bool actual = num.Match(inputString).Success();
+            Assert.False(actual);
+        }
+    }
+}
