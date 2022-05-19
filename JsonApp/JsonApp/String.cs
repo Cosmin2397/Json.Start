20df5098855823d8c2d8f1dd9391fe2d11a40804
Cosmin2397 <59839882+Cosmin2397@users.noreply.github.com>
2022-05-19 12:39:10 +0300

    Add string class, and add test for null and empty

---
+++ JsonApp/JsonApp/String.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JsonApp;
+
+namespace JsonApp
+{
+    public class String : IPattern
+    {
+        private readonly IPattern pattern;
+
+        public String()
+        {
+            pattern = new Choice();
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+    }
+}
---
+++ JsonApp/JsonAppTests/StringTests.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+using JsonApp;
+
+namespace JsonAppTests
+{
+    public class StringTests
+    {
+
+        [Theory]
+        [InlineData(null, null)]
+        [InlineData("", "")]
+        public static void CheckIfWork_WithInvalidValues(string inputString, string remainingText)
+        {
+            String jsonString = new();
+            bool actual = jsonString.Match(inputString).Success();
+            Assert.False(actual);
+            Assert.Equal(remainingText, jsonString.Match(inputString).RemainingText());
+        }
+    }
+}
