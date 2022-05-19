9f26517703445ec4dbe0f41368e4aaad56a58574
Cosmin2397 <59839882+Cosmin2397@users.noreply.github.com>
2022-05-19 12:52:36 +0300

    Add code for hex values and test it.

--- JsonApp/JsonApp/String.cs
+++ JsonApp/JsonApp/String.cs
@@ -15,8 +15,10 @@ namespace JsonApp
         {
             Character quote = new('\"');
             Choice validCharacters = new(new Range((char)32, (char)33), new Range((char)35, (char)91), new Range((char)93, (char)11093));
+            var hex = new Choice(new Range('0', '9'), new Range('a', 'f'), new Range('A', 'F'));
+            var hexSeq = new Sequence(new Character('u'), new Sequence(hex, hex, hex, hex));
             Any escapeChars = new("\"\\/bfnrt");
-            Sequence escapeCharsSeq = new(new Character('\\'), new Choice(escapeChars));
+            Sequence escapeCharsSeq = new(new Character('\\'), new Choice(escapeChars, hexSeq));
             pattern = new Sequence(quote, new Many(new Choice(validCharacters, escapeCharsSeq)), quote);
         }
 
--- JsonApp/JsonAppTests/StringTests.cs
+++ JsonApp/JsonAppTests/StringTests.cs
@@ -6,8 +6,9 @@ namespace JsonAppTests
     public class StringTests
     {
         [Theory]
+        [InlineData("\"abc\"", "")]
         [InlineData("\"\"", "")]
-        [InlineData("\"abcd\"", "")]
+        [InlineData("\"\\u1234\"", "")]
         [InlineData("\"⛅⚾\"", "")]
         [InlineData("\"\\\"\\\"\"", "")]
         [InlineData("\"\\\\\"", "")]
@@ -17,6 +18,11 @@ namespace JsonAppTests
         [InlineData("\"\\n\"", "")]
         [InlineData("\"\\r\"", "")]
         [InlineData("\"\\t\"", "")]
+        [InlineData("\"\\uabcd\"", "")]
+        [InlineData("\"\\uDEFA\"", "")]
+        [InlineData("\"\\uDEFA \\uabcd \\u1234\"", "")]
+        [InlineData("\"\\uDEFAa \\uabcd b\\u1234\"", "")]
+        [InlineData("\"a \\\\\\/\\b\\f\\n\\r\\t\\u26Be b\"", "")]
         public static void CheckIfWork_WithValidValues(string inputString, string remainingText)
         {
             String jsonString = new();
@@ -26,8 +32,18 @@ namespace JsonAppTests
         }
 
         [Theory]
+        [InlineData("a", "a")]
+        [InlineData("\"abc", "\"abc")]
+        [InlineData("abc\"", "abc\"")]
         [InlineData(null, null)]
         [InlineData("", "")]
+        [InlineData("\"a\nb\rc\"", "\"a\nb\rc\"")]
+        [InlineData("\"aa\\cc\"", "\"aa\\cc\"")]
+        [InlineData("\"\\uabc\"", "\"\\uabc\"")]
+        [InlineData("\"\\uDEF\"", "\"\\uDEF\"")]
+        [InlineData("\"\\uDEFA \\uabcd \\u123\"", "\"\\uDEFA \\uabcd \\u123\"")]
+        [InlineData("\"\\uDEFAa \\uabc b\\u1234\"", "\"\\uDEFAa \\uabc b\\u1234\"")]
+        [InlineData("\"\\\"", "\"\\\"")]
         public static void CheckIfWork_WithInvalidValues(string inputString, string remainingText)
         {
             String jsonString = new();
