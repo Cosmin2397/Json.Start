ea9a8db46f256606541f49caed88abf1fc53c190
Cosmin2397 <59839882+Cosmin2397@users.noreply.github.com>
2022-05-19 11:24:23 +0300

    Refactoring number method

--- JsonApp/JsonApp/Number.cs
+++ JsonApp/JsonApp/Number.cs
@@ -13,10 +13,10 @@ namespace JsonApp
         public Number()
         {
             Range digit = new('0', '9');
-            Sequence natural = new(new Optional(new Character('-')), new Range('1', '9'), new Many(digit));
-            Choice integer = new(natural, new Character('0'));
-            Sequence exponent = new(new Any("eE"), new Optional(new Any("+-")), new OneOrMore(digit));
-            Sequence fraction = new(new Character('.'), new OneOrMore(digit), new Optional(exponent));
+            OneOrMore digits = new(digit);
+            Choice integer = new(new Sequence(new Optional(new Character('-')), new Range('1','9'), new Many(digits)), new Character('0'));
+            Sequence exponent = new(new Any("eE"), new Optional(new Any("+-")), digits);
+            Sequence fraction = new(new Character('.'), digits);
             pattern = new Sequence(integer, new Optional(fraction), new Optional(exponent));
         }
 
