28e19cf6dd52ff271fed2e9fe7230411a44177ff
Cosmin2397 <59839882+Cosmin2397@users.noreply.github.com>
2022-05-23 20:09:57 +0300

    Create pattern for value type

--- JsonApp/JsonApp/Value.cs
+++ JsonApp/JsonApp/Value.cs
@@ -12,16 +12,16 @@ namespace JsonApp
 
         public Value()
         {
-            Character comma = new(',');
-            Character whiteSpace = new(' ');
             String str = new();
             Number num = new();
+            Character comma = new(',');
+            Character whiteSpace = new(' ');
             Choice value = new(str, num, new Text("true"), new Text("false"), new Text("null"));
             Sequence array = new(new Character('['), new List(value, new Sequence(comma, whiteSpace)), new Character(']'));
             Sequence obj = new(new Character('{'), new List(new Sequence(whiteSpace, str, whiteSpace, new Character(':'), value, whiteSpace), comma), new Character('}'));
             value.Add(array);
             value.Add(obj);
-            pattern = value;
+            pattern = new Sequence(whiteSpace, value, whiteSpace);
         }
 
         public IMatch Match(string text)
