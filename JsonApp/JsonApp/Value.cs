744799dfb4263f18dc2216ca6bafd99026e00e4e
Cosmin2397 <59839882+Cosmin2397@users.noreply.github.com>
2022-05-23 20:09:08 +0300

    Add patterns for array and obj

--- JsonApp/JsonApp/Value.cs
+++ JsonApp/JsonApp/Value.cs
@@ -12,9 +12,15 @@ namespace JsonApp
 
         public Value()
         {
+            Character comma = new(',');
+            Character whiteSpace = new(' ');
             String str = new();
             Number num = new();
             Choice value = new(str, num, new Text("true"), new Text("false"), new Text("null"));
+            Sequence array = new(new Character('['), new List(value, new Sequence(comma, whiteSpace)), new Character(']'));
+            Sequence obj = new(new Character('{'), new List(new Sequence(whiteSpace, str, whiteSpace, new Character(':'), value, whiteSpace), comma), new Character('}'));
+            value.Add(array);
+            value.Add(obj);
             pattern = value;
         }
 
