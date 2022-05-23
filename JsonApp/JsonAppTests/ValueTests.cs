1ad098021660279cdf88fb021e11650f61f74a9c
Cosmin2397 <59839882+Cosmin2397@users.noreply.github.com>
2022-05-23 20:10:26 +0300

    Add tests for value type

--- JsonApp/JsonAppTests/ValueTests.cs
+++ JsonApp/JsonAppTests/ValueTests.cs
@@ -5,6 +5,32 @@ namespace JsonAppTests
 {
     public class ValueTests
     {
+        [Theory]
+        [InlineData(" { \"age\" :20 , \"age\" :23 } ")]
+        [InlineData(" [\"name\", 3, true, \"Ovidiu\"] ")]
+        [InlineData(" 23 ")]
+        [InlineData(" \"Ion\" ")]
+        [InlineData(" true ")]
+        [InlineData(" false ")]
+        [InlineData(" null ")]
+        public static void CheckIfWork_WithValidData(string inputString)
+        {
+            Value value = new();
+            Assert.True(value.Match(inputString).Success());
+        }
 
+        [Theory]
+        [InlineData(" { \"name\" : \"Cosmin\" , \"name\" : \"Ovidiu\" ")]
+        [InlineData(" [ \"name\" ,3 ,true \"Ovidiu\"")]
+        [InlineData(" 24e ")]
+        [InlineData(" \"Ion ")]
+        [InlineData(" truex ")]
+        [InlineData(" fals ")]
+        [InlineData(" nulllllllll ")]
+        public static void CheckIfWork_WithInvalidData(string inputString)
+        {
+            Value value = new();
+            Assert.False(value.Match(inputString).Success());
+        }
     }
 }
