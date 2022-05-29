aeb46061aaa0269cf6d71916c319aef9b3ef8f35
Cosmin2397 <59839882+Cosmin2397@users.noreply.github.com>
2022-05-29 10:02:11 +0300

    Update Json main Class

--- JsonApp/Json/Program.cs
+++ JsonApp/Json/Program.cs
@@ -1,23 +1,32 @@
 ﻿using System;
+using System.IO;
 using JsonApp;
 
 namespace Json
 {
     class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
-            string input = Console.ReadLine();
-            string textFile = System.IO.File.ReadAllText(@input);
-            Value value = new();
-            bool isJson = value.Match(textFile).Success();
-            if (isJson)
+
+            if (args != null && args.Length > 0 && File.Exists(args[0]))
             {
-                Console.WriteLine("Is valid");
+                string textFile = System.IO.File.ReadAllText(args[0]);
+                Value value = new();
+                bool isJson = value.Match(textFile).Success();
+                bool isEmpty = value.Match(textFile).RemainingText() == string.Empty;
+                if (isJson && isEmpty)
+                {
+                    Console.WriteLine("Is valid");
+                }
+                else
+                {
+                    Console.WriteLine("Is invalid");
+                }
             }
             else
             {
-                Console.WriteLine("Is invalid");
+                Console.WriteLine("You need to input a valid file path!");
             }
         }
     }
