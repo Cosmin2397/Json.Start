464395c78e5f7689555cff9bbc65b831d6e9cf54
Cosmin2397 <59839882+Cosmin2397@users.noreply.github.com>
2022-06-05 08:53:49 +0300

    Add tests for count

--- Arrays/ArraysTests/IntArrayTests.cs
+++ Arrays/ArraysTests/IntArrayTests.cs
@@ -1,9 +1,19 @@
 ﻿using Arrays;
 using Xunit;
+using System;
 
 namespace ArraysTests
 {
     public class IntArrayTests
     {
+        [Fact]
+        public void CanCountsElements()
+        {
+            var arr = new IntArray();
+            arr.Add(1);
+            arr.Add(2);
+            arr.Add(3);
+            Assert.Equal(3, arr.Count());
+        }
     }
 }
